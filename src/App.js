fa676ca031ca68c788a9786e85fc8d9667d1251d
Sercantugba <sercantugba834@gmail.com>
2020-08-21 16:07:59 -0500

    updated js file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Header from './components/Header'
+import Section1 from './components/Section1'
 function App() {
   return (
     <div className="App">
-
+      <Header/>
+      <Section1/>
     </div>
   );
 }
-
 export default App;
