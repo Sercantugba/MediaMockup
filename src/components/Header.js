049c66500bac0c131236b8e80b1e544ee7e828d0
Sercantugba <sercantugba834@gmail.com>
2020-08-21 16:08:31 -0500

    cont working

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+function Header(props) {
+    return (
+        <div className='Header'>
+            <h1>The Talks</h1>
+            <h6>Interview Directory</h6>
+            <h6>World Guide</h6>
+            <img src=''/>
+            <img src=''/>
+            <img src=''/>
+        </div>
+    );
+}
+export default Header;
---
+++ src/components/Section1.js
@@ -0,0 +1,14 @@
+import React from 'react';
+function Section1(props) {
+    return (
+        <div className='Section1'>
+            <div className='Section1A'>
+                <h6>NEW INTERVIEW</h6>
+                <img src="https://the-talks.com/wp-content/uploads/2019/12/Matt-Damon-01-690x360.jpg" />
+            </div>
+            <div className='Section1B'>
+            </div>
+        </div>
+    );
+}
+export default Section1;
