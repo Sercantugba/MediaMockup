da7282b96774bee3862fd40dc9a2dcbc47fc63d1
Sercantugba <sercantugba834@gmail.com>
2020-08-25 12:11:56 -0500

    cont working media mockup

---
+++ src/components/Section2.js
@@ -0,0 +1,14 @@
+import React from 'react';
+function Section2(props) {
+    return (
+        <div>
+        <h1>Most view interview</h1>
+        <div className="Section2">
+            <img src="https://the-talks.com/wp-content/uploads/2016/05/Alicia-Keys-01.jpg"/>
+                <img src="https://the-talks.com/wp-content/uploads/2019/01/Chilly-Gonzales-01.jpg"/>
+                <img src="https://the-talks.com/wp-content/uploads/2014/09/Kevin-Kline-01.jpg"/>
+        </div>
+        </div>
+    );
+}
+export default Section2;
